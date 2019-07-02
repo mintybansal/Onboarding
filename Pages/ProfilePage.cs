2e3339415976a25cbaa5eb7480aed0bf1bb11ad3
minty bansal <mintydhall@gmail.com>
2019-07-02 14:17:12 +1200

    Update the solution file

---
+++ Pages/ProfilePage.cs
@@ -0,0 +1,35 @@
+﻿using Mars.Helpers;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mars.Pages
+{
+    class ProfilePage
+    {
+
+        public void AddnewLanguage()
+        {
+            //logic to add newlanguage
+            //Click on Add New button
+            Driver.driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/thead/tr/th[3]/div")).Click();
+
+            //Add Language
+
+            Driver.driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[1]/input")).SendKeys("French");
+
+            //Click on Language Level
+            Driver.driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[2]/select")).Click();
+
+            //Choose the language level
+            IWebElement Lang = Driver.driver.FindElements(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[2]/select/option"))[2];
+            Lang.Click();
+
+            //Click on Add button
+            Driver.driver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/div/div[3]/input[1]")).Click();
+        }
+    }
+}
